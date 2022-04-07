d4abbcd57cfc31451f3eec84c466607b6befaed6
Satria-WS <extencezone02@gmail.com>
2022-04-07 15:31:57 +0700

    route
    create chaining routes

--- app.js
+++ app.js
@@ -10,31 +10,73 @@ const app = express();
 //templating using ejs
 app.set("view engine", "ejs");
 //body-parer in onrder to pass our request
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 //going to use the public directory to store out static files such as images css code
 app.use(express.static("public"));
 //Connect to mongodb
-mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  useNewUrlParser: true,
+});
 //create db articleSchema
 const articleSchema = {
   title: "String",
-  content: "String"
+  content: "String",
 };
 //crate model name
 const Article = mongoose.model("Article", articleSchema);
-//Get routes, Fetches all the entire collection of articles
-app.get("/articles", (req, res) => {
-  //syntax code: <ModelName>.find({conditions},function(err,results){//user the found results doct});;
-  Article.find((err, foundArticles) => {
-    //console.log(foundArticles);
-    if (!err) {
-      //res.send log to client
-      res.send(foundArticles);
-    } else {
-      res.send(err);
-    }
+//create chain route
+app.route("/articles")
+  .get((req, res) => {
+    //syntax code: <ModelName>.find({conditions},function(err,results){//user the found results doct});;
+    Article.find((err, foundArticles) => {
+      //console.log(foundArticles);
+      if (!err) {
+        //res.send log to client
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post((req, res) => {
+    console.log(req.body.title);
+    console.log(req.body.content);
+
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+
+    newArticle.save(function (err) {
+      if (!err) {
+        res.send("Run Successfully add a new article");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .delete((req, res) => {
+    Article.deleteMany((err) => {
+      if (!err) {
+        res.send("Sucessfull deleted all articles");
+      } else {
+        res.send(err);
+      }
+    });
   });
-});
+
+//Get routes, Fetches all the entire collection of articles
+app.get("/articles");
+
+//Post routes for creates one new article
+app.post("/articles");
+
+//Delete routes
+app.delete("/articles");
 
 //The last part set up our app to listen on port 3000
 app.listen(3000, () => {
